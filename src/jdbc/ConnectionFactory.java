0769f3f0fca6acc768872bc6669c7d997da6b496
leandrocrf <lemottajr@hotmail.com>
2021-06-22 21:08:33 -0300

    fix

--- src/jdbc/ConnectionFactory.java
+++ src/jdbc/ConnectionFactory.java
@@ -10,7 +10,7 @@ public class ConnectionFactory {
 	private static final String DRIVE = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://database-poo.ce0bmfb0qtle.us-east-1.rds.amazonaws.com:3306/POO_BD?useTimezone=true&serverTimezone=UTC"; 
 	private static final String USER = "admin"; 
-	private static final String SENHA = "Leandro97"; 
+	private static final String SENHA = "*********"; 
 	
 	
 	public static Connection getConnection() {
